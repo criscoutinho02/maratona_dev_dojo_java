fc8adc07d7b9708a8fbdaea73703f15e815b2dea
criscoutinho02 <cris.coutinho02@gmail.com>
2021-04-03 00:30:36 -0300

    Aula 62:  Interfaces

---
+++ src/br/com/abc/javacore/Minterfaces/classes/Produto.java
@@ -0,0 +1,73 @@
+package br.com.abc.javacore.Minterfaces.classes;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Produto implements Tributavel , Transportavel{
+
+    private String nome;
+    private double peso;
+    private double preco;
+    private double precoFinal;
+    private double valorFrete;
+
+
+    public Produto(String nome, double peso, double preco) {
+        this.nome = nome;
+        this.peso = peso;
+        this.preco = preco;
+    }
+
+    @Override
+    public void calcularImposto() {
+        precoFinal = this.preco + (this.preco * IMPOSTO);
+
+    }
+
+    @Override
+    public void calcularFrete() {
+        this.valorFrete = this.preco/peso * 0.1;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", peso=" + peso +
+                ", preco=" + preco +
+                ", Preço Final com Imposto =" + precoFinal +
+                ", valorFrete=" + valorFrete +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getPrecoFinal() {
+        return precoFinal;
+    }
+
+    public double getValorFrete() {
+        return valorFrete;
+    }
+}
