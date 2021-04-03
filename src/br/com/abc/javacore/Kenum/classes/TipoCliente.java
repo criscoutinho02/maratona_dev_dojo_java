c089068416446c40d3bc8a7485b6b8c654d8c9b6
criscoutinho02 <cris.coutinho02@gmail.com>
2021-04-02 23:48:58 -0300

    Aula 59:  Enumeração pt03

--- src/br/com/abc/javacore/Kenum/classes/TipoCliente.java
+++ src/br/com/abc/javacore/Kenum/classes/TipoCliente.java
@@ -2,7 +2,35 @@ package br.com.abc.javacore.Kenum.classes;
 
 public enum TipoCliente {
 
-    PESSOA_FISICA,
-    PESSOA_JURIDICA;
+    PESSOA_FISICA(1 , "Pessoa Física"),
+    //constant specific class body
+    PESSOA_JURIDICA(2 , "Pessoa Jurídica"){
+        public String getId(){
+            return "A";
+        }
+    }
+
+    ;
+
+    private int tipo;
+    private String nome;
+
+    TipoCliente(int tipo , String nome) {
+        this.tipo = tipo;
+        this.nome = nome;
+    }
+
+    public String getId(){
+        return "B";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
 
 }
--- src/br/com/abc/javacore/Kenum/test/ClienteTest.java
+++ src/br/com/abc/javacore/Kenum/test/ClienteTest.java
@@ -3,10 +3,15 @@ package br.com.abc.javacore.Kenum.test;
 import br.com.abc.javacore.Kenum.classes.Cliente;
 import br.com.abc.javacore.Kenum.classes.TipoCliente;
 
+import java.util.concurrent.TimeUnit;
+
 public class ClienteTest {
 
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("Cristina" , TipoCliente.PESSOA_FISICA);
+        Cliente cliente = new Cliente("Cristina" , TipoCliente.PESSOA_FISICA , Cliente.TipoPagamento.APRAZO);
         System.out.println(cliente);
+        System.out.println(TipoCliente.PESSOA_JURIDICA.getId());
+        System.out.println(TipoCliente.PESSOA_FISICA.getId());
+
     }
 }
