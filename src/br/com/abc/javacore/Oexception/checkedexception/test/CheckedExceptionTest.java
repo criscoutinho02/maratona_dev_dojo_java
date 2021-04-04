824c6e677f7fd4757ff15dfe9b99a77bcd2449a8
criscoutinho02 <cris.coutinho02@gmail.com>
2021-04-04 00:29:53 -0300

    Aula 71: Exceptions: bloco finally pt 05

--- src/br/com/abc/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
+++ src/br/com/abc/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
@@ -6,17 +6,36 @@ import java.io.IOException;
 public class CheckedExceptionTest {
 
     public static void main(String[] args) {
-        criarArquivo();
+        abrirArquivo();
 
     }
 
-    public static void criarArquivo() {
+    public static void criarArquivo() throws IOException {
         File file = new File("teste.txt");
         try {
             System.out.println(file.createNewFile());
             System.out.println("Arquivo criado");
         } catch (IOException e) {
             e.printStackTrace();
+            throw e;
         }
+    }
+
+    public static String abrirArquivo() {
+        File file = new File("teste.txt");
+        try {
+            System.out.println("ABRINDO UM ARQUIVO");
+            System.out.println("Executando a leitura do arquivo");
+//            throw new Exception("Erro ao ler o arquivo");
+//            System.out.println("Escrevendo em um arquivo");
+            return "valor";
+        } catch (Exception e) {
+            System.out.println("Dentro do catch");
+            e.printStackTrace();
+        } finally {
+            System.out.println("FECHAR O ARQUIVO");
+
+        }
+        return null;
     }
 }
--- src/br/com/abc/javacore/Oexception/runtimeexception/test/RuntimeExceptionTest.java
+++ src/br/com/abc/javacore/Oexception/runtimeexception/test/RuntimeExceptionTest.java
@@ -3,28 +3,22 @@ package br.com.abc.javacore.Oexception.runtimeexception.test;
 public class RuntimeExceptionTest {
 
     public static void main(String[] args) {
-
-//        Object o = null ;
-//        System.out.println(o.toString());
-
-
-//        int a = 10;
-//        int b = 0;
-//        if(b!=0) {
-//            int c = a / b;
-//            System.out.println(c);
-//        }
-        try{
-            int[] a = new int[2];
-            System.out.println(a[2]);
-        }catch (ArrayIndexOutOfBoundsException e){
-            e.printStackTrace();
+        try {
+            divisao(10, 0);
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
         }
-        System.out.println("Após bloco catch");
 
 
 
     }
 
+    private static void divisao(int num1 , int num2){
+        if(num2==0){
+            throw new IllegalArgumentException("Passe um valor diferente de 0 para num2");
+        }
+        int result = num1/num2;
+        System.out.println(result);
+    }
 
 }
